012f8c9e5ebce157b3ea2ad85ba1448e2b5b4086
Kaden Hansen <kadendeanhansen@gmail.com>
2022-01-19 19:27:02 -0500

    spawn snake at game start

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
 let currentFrame = 0
 let previousFrame = 0
-let snakeSpeed = 3
+let snakeSpeed = 1
+let gameWorld = document.querySelector(".game-world")
+let snake = { x: 13, y: 13 }
 
 function main(frame) {
-
+    
     window.requestAnimationFrame(main)
     currentFrame = (frame - previousFrame) / 1000
     if (currentFrame < 1 / snakeSpeed) return
     
     previousFrame = frame
-    console.log(currentFrame)
+    createSnake(gameWorld)
 }
 window.requestAnimationFrame(main)
+
+
+function createSnake(grid) {
+    let snakeSection = document.createElement("div")
+    snakeSection.style.gridRowStart = snake.x
+    snakeSection.style.gridColumnStart = snake.y
+    snakeSection.classList.add("snake")
+    grid.append(snakeSection)
+}
