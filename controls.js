5a770d77dab665a95a752f74ce08f25ed940831a
Kaden Hansen <kadendeanhansen@gmail.com>
2022-01-22 12:05:53 -0500

    Add forward only direction so the snake head can only turn left or right from current direction

--- controls.js
+++ controls.js
@@ -1,18 +1,27 @@
 let snakeDirection = { x: 0, y: 0 }
+let previousDirection = null
 
 window.addEventListener("keydown", e => {
     switch (e.key) {
         case "ArrowUp": 
+            if (previousDirection === "ArrowDown") return
             snakeDirection = { x: 0, y: -1 }
+            previousDirection = "ArrowUp"
             break
         case "ArrowDown": 
+            if (previousDirection === "ArrowUp") return
             snakeDirection = { x: 0, y: 1 }
+            previousDirection = "ArrowDown"
             break
         case "ArrowLeft": 
+            if (previousDirection === "ArrowRight") return
             snakeDirection = { x: -1, y: 0 }
+            previousDirection = "ArrowLeft"
             break
         case "ArrowRight": 
+            if (previousDirection === "ArrowLeft") return
             snakeDirection = { x: 1, y: 0 }
+            previousDirection = "ArrowRight"
             break
     }
 })
