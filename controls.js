c06ff46fa124c9655938cc82108f7842b415e880
Kaden Hansen <kadendeanhansen@gmail.com>
2022-01-22 12:08:35 -0500

    Fix return ins switch case to break

--- controls.js
+++ controls.js
@@ -4,22 +4,22 @@ let previousDirection = null
 window.addEventListener("keydown", e => {
     switch (e.key) {
         case "ArrowUp": 
-            if (previousDirection === "ArrowDown") return
+            if (previousDirection === "ArrowDown") break
             snakeDirection = { x: 0, y: -1 }
             previousDirection = "ArrowUp"
             break
         case "ArrowDown": 
-            if (previousDirection === "ArrowUp") return
+            if (previousDirection === "ArrowUp") break
             snakeDirection = { x: 0, y: 1 }
             previousDirection = "ArrowDown"
             break
         case "ArrowLeft": 
-            if (previousDirection === "ArrowRight") return
+            if (previousDirection === "ArrowRight") break
             snakeDirection = { x: -1, y: 0 }
             previousDirection = "ArrowLeft"
             break
         case "ArrowRight": 
-            if (previousDirection === "ArrowLeft") return
+            if (previousDirection === "ArrowLeft") break
             snakeDirection = { x: 1, y: 0 }
             previousDirection = "ArrowRight"
             break
